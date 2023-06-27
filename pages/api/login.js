5dc46120a79f5d4f357dc9734a58f3d6e5949dcf
Rohit Verma <28vrohit@gmail.com>
2023-06-27 23:01:02 +0530

    login signup done

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,18 @@
-export default function handler(req, res) {
+import dbConnect from "../../lib/mongodb";
+import User from "../../modal/User";
+
+export default async function handler(req, res) {
+  await dbConnect();
+  const body = req.body;
+
   try {
-    res.status(200).json({ name: "login" });
-  } catch (error) {}
+    let singlePerson = await User.findOne({ email: body.email }).exec();
+    if (singlePerson) {
+      res.status(200).json({ success: true, singlePerson });
+    } else {
+      res.status(400).json({ success: false, error: "User not already exit" });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 }
--- pages/api/register.jsx
+++ pages/api/register.jsx
@@ -5,18 +5,23 @@ export default async function handler(req, res) {
   await dbConnect();
 
   const body = req.body;
-  const user = await new User(
-    {
-      firstName: body.firstName,
-      lastName: body.lastName,
-      fullName: body.fullName,
-      email: body.email,
-      password: body.password,
-    },
-    {
-      version: false,
+  const user = await new User({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    fullName: body.fullName,
+    email: body.email,
+    password: body.password,
+  });
+
+  try {
+    let singlePerson = await User.findOne({ email: body.email }).exec();
+    if (singlePerson) {
+      res.status(400).json({ success: false, error: "User already exit" });
+    } else {
+      const newuser = await user.save();
+      res.status(200).json({ success: true, newuser });
     }
-  );
-  const newuser = await user.save();
-  res.status(200).json({ newuser });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 }
