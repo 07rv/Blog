c9bbcee4204459d2697af69600c5c403b0ad2c3e
Rohit Verma <28vrohit@gmail.com>
2023-06-28 22:56:00 +0530

    homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Login from "@/components/account/Login";
+import HomePage from "@/components/home/HomePage";
 import DataProvider from "@/context/DataProvider";
 
 export default function Home() {
   return (
     <DataProvider>
       <Login />
+      <HomePage />
     </DataProvider>
   );
 }
