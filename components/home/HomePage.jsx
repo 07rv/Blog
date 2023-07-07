9d8199d1b34a45b5fa24404b14fd2d6bff89717a
Rohit Verma <28vrohit@gmail.com>
2023-07-07 16:20:38 +0530

    code

--- components/home/HomePage.jsx
+++ components/home/HomePage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-import withAuth from "@/pages/api/withAuth";
+import Banner from "../banner/Banner";
+import Categories from "./Categories";
+import { Grid } from "@mui/material";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Banner />
+      <Grid container>
+        <Grid item lg={2} sm={2} xs={12}>
+          <Categories />
+        </Grid>
+        <Grid container item xs={12} sm={10} lg={2}>
+          Posts
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
-export default withAuth(HomePage);
+export default HomePage;
